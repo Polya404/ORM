563bb58bcc0ab8d8176165f82976936f04fa661d
Polya404 <polyapolya404@gmail.com>
2022-08-10 21:59:02 +0300

    Add image.png

--- src/main/java/knubisoft/Main.java
+++ src/main/java/knubisoft/Main.java
@@ -1,5 +1,6 @@
 package knubisoft;
 
+import knubisoft.classes.Person;
 import knubisoft.classes.Person2;
 
 import java.io.File;
@@ -9,7 +10,8 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        URL url = Main.class.getClassLoader().getResource("MOCK_DATA.json");
-        List<Person2> result = new ORM().transform(new File(url.toURI()), Person2.class);
+        URL url1 = Main.class.getClassLoader().getResource("dataset.xml");
+        URL url2 = Main.class.getClassLoader().getResource("MOCK_DATA.json");
+        List<Person> result = new ORM().transform(new File(url1.toURI()), Person.class);
     }
 }
--- src/main/java/knubisoft/parsingStrategy/XMLParsingStrategy.java
+++ src/main/java/knubisoft/parsingStrategy/XMLParsingStrategy.java
@@ -1,11 +1,24 @@
 package knubisoft.parsingStrategy;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import knubisoft.Table;
+import lombok.SneakyThrows;
+
 
 public class XMLParsingStrategy implements ParsingStrategy {
+    JSONParsingStrategy jsonParsingStrategy = new JSONParsingStrategy();
+
+    @SneakyThrows
     @Override
     public Table parseToTable(String content) {
         // TODO
-        return null;
+        XmlMapper xmlMapper = new XmlMapper();
+        JsonNode tree = xmlMapper.readTree(content);
+        int i = String.valueOf(tree.fields().next()).indexOf("[");
+        String str = String.valueOf(tree.fields().next()).substring(i);
+
+        return jsonParsingStrategy.parseToTable(str);
     }
+
 }
